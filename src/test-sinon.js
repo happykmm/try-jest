b8994be4fc349355825c1a73b3f61ec1196b814d
FAREAST\yuzhon <yuzhon@microsoft.com>
2018-01-16 14:03:02 +0800

    test sinon

---
+++ src/test-sinon.js
@@ -0,0 +1,5 @@
+const sinon = require('sinon');
+
+const spy = sinon.spy();
+
+console.log(spy())
--- test/sum2.test.js
+++ test/sum2.test.js
@@ -2,17 +2,37 @@ jest.mock('../src/sum');
 
 const sum = require('../src/sum');
 
-describe('sum', () => {  
-  test('adds 1 + 2', () => {
-    sum(1, 2);
-    foo = 'foo';
-    console.log(global.foo);
-    console.log(global.bar);
-    expect(sum).toHaveBeenCalledTimes(1);
+function LeakingClass() {
+}
+
+global.leaks = [];
+setInterval(function() {
+  for (var i = 0; i < 100; i++) {
+    leaks.push(new LeakingClass());
+  }
+}, 1);
+
+describe('test-suite', () => {  
+  test('test-case', () => {
+    const data = {
+      1: 'a',
+      2: 'b',
+    };
+    sum(15, 'foo', { key: 'value' });
+    global.foo = 'foo';
+    console.log('global.bar = ', global.bar);
+    expect(sum).toMatchSnapshot();
+    setTimeout(() => {
+      console.log(JSON.stringify(data));
+    }, 10000);
   });
 
-  test('adds 1 + 3', () => {
+  test('adds 1 + 3', (done) => {
     global.bar = 'bar';
-    console.log(global.foo);
+    console.log('global.foo = ', global.foo);
+    setTimeout(() => {
+      console.log(leaks.length);
+      done();
+    }, 1000);
   });
 });
