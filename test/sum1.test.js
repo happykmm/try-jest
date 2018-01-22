88cb79674931f6b54746f8d6f5ff8f3b848556fa
FAREAST\yuzhon <yuzhon@microsoft.com>
2018-01-22 09:57:38 +0800

    test sandbox

--- test/sum1.test.js
+++ test/sum1.test.js
@@ -2,16 +2,7 @@ jest.mock('../src/sum');
 
 const sum = require('../src/sum');
 
-describe('sum', () => {  
-  test('add 1 + 1', () => {
-    sum(1, 1);
-    sum.baz = 'baz';
-    global.baz = 'baz';
-    console.log(sum.foo);
-    console.log(global.foo);
-    expect(sum).toHaveBeenCalledTimes(1);
-  });
-
+describe('sum1', () => {  
   test('add 1 + 1', () => {
     sum(1, 1);
     sum.foo = 'foo';
